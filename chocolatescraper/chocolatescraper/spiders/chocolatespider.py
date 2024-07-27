b161909448b8d0213758c9f827f475b14e51753c
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-07-28 00:30:56 +0700

    Add Itemloaders

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -1,5 +1,7 @@
 import scrapy
 from chocolatescraper.items import ChocolateProduct
+from chocolatescraper.itemloaders import ChocolateProductLoader
+
 
 class ChocolatespiderSpider(scrapy.Spider):
     name = "chocolatespider"
@@ -8,17 +10,18 @@ class ChocolatespiderSpider(scrapy.Spider):
 
     def parse(self, response):
         products = response.css('product-item')
-        product_item = ChocolateProduct()
         for product in products:
-          
-            product_item['name']  =  product.css('a.product-item-meta__title::text').get()
-            product_item['price'] = product.css('span.price').get().replace('<span class="price price--highlight">\n              <span class="visually-hidden">Sale price</span>', '').replace('</span>', '')
-            product_item['url']   = product.css('div.product-item-meta a').attrib['href']    
+            chocolate = ChocolateProductLoader(
+                item=ChocolateProduct(), selector=product)
+            chocolate.add_css('name', "a.product-item-meta__title::text")
+            chocolate.add_css(
+                'price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+            yield chocolate.load_item()
 
-            yield product_item
+            yield chocolate.load_item()
 
         next_page = response.css('[rel="next"] ::attr(href)').get()
-        if next_page is not None : 
+        if next_page is not None:
             next_page_url = "https://chocolate.co.uk" + next_page
             yield response.follow(next_page_url, callback=self.parse)
-
