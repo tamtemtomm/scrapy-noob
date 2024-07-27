5f902133b449e6dbbfca58c7e0d0b1a4dc56a7ef
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-07-28 00:14:23 +0700

    Add Next Page Functionalities

--- chocolatescraper/chocolatescraper/items.py
+++ chocolatescraper/chocolatescraper/items.py
@@ -6,7 +6,9 @@
 import scrapy
 
 
-class ChocolatescraperItem(scrapy.Item):
+class ChocolateProduct(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
     pass
--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from chocolatescraper.items import ChocolateProduct
 
 class ChocolatespiderSpider(scrapy.Spider):
     name = "chocolatespider"
@@ -8,11 +8,17 @@ class ChocolatespiderSpider(scrapy.Spider):
 
     def parse(self, response):
         products = response.css('product-item')
+        product_item = ChocolateProduct()
         for product in products:
-            yield{
-                'name': product.css('a.product-item-meta__title::text').get(),
-                'price': product.css('span.price').get().replace('<span class="price price--highlight">\n              <span class="visually-hidden">Sale price</span>', '').replace('</span>', ''),
-                'url': product.css('div.product-item-meta a').attrib['href']    
-            }
-        
-        pass
+          
+            product_item['name']  =  product.css('a.product-item-meta__title::text').get()
+            product_item['price'] = product.css('span.price').get().replace('<span class="price price--highlight">\n              <span class="visually-hidden">Sale price</span>', '').replace('</span>', '')
+            product_item['url']   = product.css('div.product-item-meta a').attrib['href']    
+
+            yield product_item
+
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+        if next_page is not None : 
+            next_page_url = "https://chocolate.co.uk" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+
